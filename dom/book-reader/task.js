c12ebcbbbf2fcb09436d47db0b53e9e9f25ceaa5
Alla Smelkova <87067294+Smelkovaalla@users.noreply.github.com>
2022-06-27 11:28:31 +0300

    Update task.js

---
+++ dom/book-reader/task.js
@@ -0,0 +1,27 @@
+const fontSizes = Array.from(document.querySelectorAll('.font-size'));
+
+function onClick(event) {
+    event.preventDefault();
+    fontSizes.forEach((item) => {
+        if (item.classList.contains("font-size_active")) {
+            item.classList.remove("font-size_active");
+            book.classList.remove("font-size_small");
+            book.classList.remove("font-size_big");
+        };
+    });
+    
+    if (this.classList.contains("font-size_small")) {
+        this.classList.add("font-size_active");
+        book.classList.add("font-size_small");
+    } else if (this.classList.contains("font-size_big")) {
+        this.classList.add("font-size_active");
+        book.classList.add("font-size_big");
+    } else if (!this.classList.contains("font-size_big") && !this.classList.contains("font-size_small")) {
+        this.classList.add("font-size_active");
+    };
+    };
+
+  
+fontSizes.forEach((item) => {
+    item.addEventListener('click', onClick);
+});
