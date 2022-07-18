f62bd71e2c17cee2935267d8268433a36ef26f73
Alla Smelkova <87067294+Smelkovaalla@users.noreply.github.com>
2022-07-18 18:42:18 +0300

    Update task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,16 +1,17 @@
 const reveal = document.querySelectorAll('.reveal')
-function isVisible(el) {
-    const {top, bottom } = el.getBoundingClientRect()
-    if (bottom < 0) {
-        el.classList.remove('reveal_active');
-    };
-    if (top > window.innerHeight){
-        el.classList.remove('reveal_active');
-    };
-    el.classList.add('reveal_active');  
+function isVisible(elem) {
+    const viewportHeight = window.innerHeight;
+    const revealTop = elem.getBoundingClientRect().top;
+
+    return ((revealTop < viewportHeight) && (revealTop > 0)) ? true : false;   
 };
-
-
-for(i = 0; i < reveal.length; i++) {
-    isVisible(reveal[i])
+function checkReveals () {
+    reveal.forEach((reveal) => {
+    if (isVisible(reveal)) {
+      reveal.classList.add("reveal_active");
+    } else {
+      reveal.classList.remove("reveal_active");
+    }
+  })
 }
+window.onscroll =  checkReveals
